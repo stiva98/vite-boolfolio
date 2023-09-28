099d93fa91c32e5b1f41c0767ead57ab09807cda
stiva98 <samustiva9810@gmail.com>
2023-09-28 15:49:34 +0200

    ADD funzionamento router error 404

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+
+    },
+    methods: {
+        
+    },
+    mounted() {
+        console.log('NotFoundPage')
+    }
+};
+
+</script>
+<template>
+    <h1>
+        Error 404 - Qualcosa hai sbagliato caro utente
+    </h1>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from './pages/HomePage.vue';
 import AboutPage from './pages/AboutPage.vue';
+import NotFoundPage from './pages/NotFoundPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
             path: '/chi-siamo',
             name: 'about',
             component: AboutPage,
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'notfound',
+            component: NotFoundPage,
         }
     ]
 });
