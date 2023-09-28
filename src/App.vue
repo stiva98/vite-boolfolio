c8eba7348171bd68dfaf14aa4a3fe0458653e8dc
stiva98 <samustiva9810@gmail.com>
2023-09-28 14:18:12 +0200

    ADD

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <MainComponent />
+
+  <FooterComponent />
+</template>
+
+<style lang="scss">
+@use "assets/scss/main";
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "MainComponent",
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="title text-success text-center">
+                <h1>
+                    Laravel Boolfolio - API
+                </h1>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
+main {
+    padding: $main-padding;
+    .title {
+    padding-bottom: 20px;
+}
+}
+</style>
